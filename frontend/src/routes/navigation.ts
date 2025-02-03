e20f6ecaa5194118a22ef50efb89184e7cb9932f
chiheb ben cheikh <bencheikh.chiheb@gmail.com>
2025-02-03 22:51:44 +0100

    Add routing

---
+++ frontend/src/routes/navigation.ts
@@ -0,0 +1,6 @@
+export const navigationRoutes = [
+  { name: "Home", href: "/" },
+  { name: "login", href: "/login" },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+];
