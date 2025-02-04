520677e3e7b5dd0f99377d35001956062986ad4f
chiheb ben cheikh <bencheikh.chiheb@gmail.com>
2025-02-04 22:31:29 +0100

    Add register page

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from "../features/auth/pages/LoginPage";
+import RegisterPage from "../features/auth/pages/RegisterPage";
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login/bikerXpert" element={<LoginPage />} />
+        <Route path="/register/bikerXpert" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
